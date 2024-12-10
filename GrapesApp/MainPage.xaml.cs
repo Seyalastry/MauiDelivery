6d53e98bd7a800022d07dc8863e5e3a77d6fb0b0
se <Lenovo@DESKTOP-NF9B4UM>
2024-12-10 19:38:06 +0530

    GrapesApp developed

---
+++ GrapesApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using GrapesApp.Models;
+
+namespace GrapesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<GrapesInfo> GrapesInformation { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            GrapesInformation = new List<GrapesInfo>
+            {
+                new GrapesInfo
+                {
+                    Micronutrient = "Vitamin C",
+                    Percentage = 101
+                },
+                new GrapesInfo
+                {
+                    Micronutrient = "Vitamin A",
+                    Percentage = 19
+                },
+                new GrapesInfo
+                {
+                    Micronutrient = "Calcium",
+                    Percentage = 2
+                },
+                new GrapesInfo
+                {
+                    Micronutrient = "Magnesium",
+                    Percentage = 5
+                },
+                new GrapesInfo
+                {
+                    Micronutrient = "Potassium",
+                    Percentage = 5
+                },
+            };
+            this.BindingContext = this;
+        }
+
+        
+    }
+
+}
---
+++ GrapesApp/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Xe.AcrylicView;
+
+namespace GrapesApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseAcrylicView()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Roboto-Bold.ttf","Robotobold ");
+                    fonts.AddFont("Roboto-Regular.ttf", "Robotoregular");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
